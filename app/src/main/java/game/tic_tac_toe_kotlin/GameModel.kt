45bf6798a0573f78125e69f2d62c544f877fe626
Dima Lebedev <86880842+qqx-d@users.noreply.github.com>
2024-04-19 23:34:51 +0300

    Update GameModel.kt

--- app/src/main/java/game/tic_tac_toe_kotlin/GameModel.kt
+++ app/src/main/java/game/tic_tac_toe_kotlin/GameModel.kt
@@ -2,6 +2,7 @@ package game.tic_tac_toe_kotlin
 
 import kotlin.random.Random
 
+// The main game model that is used to save and transfer data to cloud storage
 data class GameModel(
     var gameID: String = "-1",
     var gameField: MutableList<String> = mutableListOf("","","","","","","","",""),
@@ -15,6 +16,7 @@ data class GameModel(
     var playerOName : String = ""
 )
 
+// A class for game state inentification
 enum class GameState {
     CREATED,
     JOINED,
